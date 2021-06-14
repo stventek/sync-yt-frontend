f1207e7c2bc4a740468966035d56ba519100240a
xenizs <xenizs@protonmail.com>
2021-06-14 19:32:04 -0400

    creating navbar component

--- src/views/room/room.tsx
+++ src/views/room/room.tsx
@@ -1,7 +1,10 @@
+import NavBar from '../../components/navbar/navbar';
+
 export default function Room(props : {room: string}){
+ 
     return (
         <div>
-            {props.room}
+            <NavBar/>
         </div>
     )
 }
