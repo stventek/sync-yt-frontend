c416fe0ca9df7d025e18fe6141cc746692b5ca93
xenizs <xenizs@protonmail.com>
2021-06-16 19:46:52 -0400

    remove unnecessary styles in player controls component

--- src/components/player/player-controls.tsx
+++ src/components/player/player-controls.tsx
@@ -1,21 +1,11 @@
 import Paper from '@material-ui/core/Paper';
 import Slider from '@material-ui/core/Slider';
-import makeStyles from '@material-ui/core/styles/makeStyles';
 import msToTime from '../../utilities/ms-to-time';
 import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import PauseIcon from '@material-ui/icons/Pause';
 import {useState} from 'react';
-
-const useStyles = makeStyles((theme) => {
-    return {
-        controls: {
-           
-        }
-    }
-});
-
 
 function ValueLabelComponent(props: any) {
     const { children, open, value } = props;
@@ -33,11 +23,10 @@ type controlsProps = {
 }
 
 export default function PlayerControls(props: controlsProps){
-    const classes = useStyles();
     const [state, setState] = useState({pause: true});
     return (
         <div>
-            <Paper className={classes.controls}>
+            <Paper>
                 <Slider style={{margin: 0}} defaultValue={props.start} color="secondary" ValueLabelComponent={ValueLabelComponent} min={0} max={1000 * 60 * 60} valueLabelDisplay="auto" valueLabelFormat={x => msToTime(x)}/>
                 <IconButton>
                     {state.pause ? <PlayArrowIcon/> : <PauseIcon/>}
